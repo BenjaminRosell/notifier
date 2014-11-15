aef5d360c8264a9449fa997d008018263eea7395
King <wangzhenzhen@Wangs-Mac-mini.local>
2014-11-15 09:45:36 +0800

    .

--- WrapperParse/YYYViewController.h
+++ WrapperParse/YYYViewController.h
@@ -2,8 +2,8 @@
 //  YYYViewController.h
 //  WrapperParse
 //
-//  Created by Wang MeiHua on 11/15/14.
-//  Copyright (c) 2014 Wang. All rights reserved.
+//  Created by BestDev on 11/15/14.
+//  Copyright (c) 2014 BestDev. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
